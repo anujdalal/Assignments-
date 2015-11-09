333601ae59c0c12d3af279797cad455b2589ed88
anujdalal <anujd744@gmail.com>
2015-11-09 15:03:03 -0500

    removed irrelevant folders

--- workspace/Assignment 2/src/rsvInterface.java
+++
@@ -1,9 +0,0 @@
-
-public interface rsvInterface {
- 
-	public void list (int a, int b) throws java.rmi.RemoteException;
-	
-	public void passengerlist (String a, String b, int c) throws java.rmi.RemoteException;
-
-	public void reserve (String c, String d, int e) throws java.rmi.RemoteException;
-}
