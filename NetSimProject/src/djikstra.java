e11c5f62a4b938f03fa33df95b79386ceaa27e2d
anujdalal <anujd744@gmail.com>
2015-12-03 18:23:01 -0500

    Added new project

---
+++ NetSimProject/src/djikstra.java
@@ -0,0 +1,30 @@
+
+import java.io.*;
+import java.util.HashSet;
+import java.util.Iterator;
+import java.util.List;
+import java.util.ArrayList;
+import java.util.Set;
+import java.util.Map;
+import java.util.concurrent.ConcurrentHashMap;
+import java.util.concurrent.ConcurrentMap;
+
+public class djikstra {
+
+	private int metric = 0; //SPF final metric for each link
+	private int Cij = 0;  // link capacity, as given in file
+	
+	public int spf (int metric, int Cij){
+		
+		//define number of nodes, i.
+		for(int i = 26; i < 27; i++){
+			
+			
+		}
+		
+		return metric;
+		
+	}
+	
+	
+}
---
+++ NetSimProject/src/topology.java
@@ -0,0 +1,14 @@
+
+import java.io.*;
+import java.util.HashSet;
+import java.util.Iterator;
+import java.util.List;
+import java.util.ArrayList;
+import java.util.Set;
+import java.util.Map;
+import java.util.concurrent.ConcurrentHashMap;
+import java.util.concurrent.ConcurrentMap;
+
+public class topology {
+
+}
