aeef90359e539e263188ea1a3effaaa98d1e30b6
Anuj <anujd744@gmail.com>
2016-03-17 13:07:50 -0400

    Added option to modify output connector of a flow (only thing needed right now).

--- NET Senior Project/FlowCreator.py
+++ NET Senior Project/FlowCreator.py
@@ -1,42 +1,55 @@
-from xml.etree.ElementTree import Element
-from xml.dom import minidom
-from subprocess import call
-import xml.etree.ElementTree as etree
 import os
 # this function constructs flows for each device, and destination address
 def createflows():
-
-    # These variables will be changed for each flow.
-    node = 'Enter the device you want to modify (openflow:x): '
-    flowname = 'Flow Name: '
-    flid = 'Flow ID: '
-    flpriority = 'Flow Priority: '
-    transport = 'TCP/UDP: '
-    ipv4 = 'Destination IP: '
-    outint = 'Egress interface: '
     true = 1
-
+    count = 0
     while (true == 1):
-        dev = 'openflow:11' #raw_input(node)
-        print 'Please fill in the following to add a flow'
-        flname = raw_input(flowname)
-        flowid = raw_input(flid)
-        flowpriority = raw_input(flpriority)
-        ipprot = raw_input(transport)
-        ipaddr = raw_input(ipv4)
-        output = raw_input(outint)
-	os.system('''curl -u admin:admin -H 'Content-type: application/json' -X PUT -d '{
-                  "flow": [{"id": "'''+flowid+'''", "match": { "ethernet-match": { "ethernet-type": { "type": "2048"}}, "ip-match": {"ip-protocol": "'''+ipprot+'''"},
-                  "ipv4-destination": "'''+ipaddr+'''"}, "instructions": { "instruction": [{ "order": "0",
-                  "apply-actions": { "action": [{ "order": "0", "output-action": { "output-node-connector": "'''+output+'''",
-                  "max-length": "60"}}]}}]}, "flow-name": "'''+flname+'''", "installHw": "true", "strict": "false", "priority": "'''+flowpriority+'''",
-                  "idle-timeout": "0", "hard-timeout": "0", "table_id": "0"}]}' 'http://localhost:8181/restconf/config/opendaylight-inventory:nodes/node/'''+dev+'''/flow-node-inventory:table/0/flow/'''+flowid+"'"'')
-       
-	addmore = 'Add another flow?'
-        response = raw_input(addmore)
-        if (response == 'y' or 'Y'):
-            true = 1
-        else:
-            true = 0
+        dev = raw_input('Enter the device you want to modify (openflow:x): ')
+        request = raw_input('(A)dd a flow, or (m)odify an existing flow?')
 
+        if (request == 'a' or 'A'):
+            print 'Please fill in the following to add a flow'
+            flowname = raw_input('Flow Name: ')
+            flowid = raw_input('Flow ID: ')
+            flowpriority = raw_input('Flow Priority: ')
+            ipprot = raw_input('TCP/UDP: ')
+            ipaddr = raw_input('Destination IP: ')
+            outint = raw_input('Egress interface: ')
+
+            os.system('''curl -u admin:admin -H 'Content-type: application/json' -X PUT -d '{
+                      "flow": [{"id": "'''+flowid+'''", "match": { "ethernet-match": { "ethernet-type": { "type": "2048"}}, "ip-match": {"ip-protocol": "'''+ipprot+'''"},
+                      "ipv4-destination": "'''+ipaddr+'''"}, "instructions": { "instruction": [{ "order": "0",
+                      "apply-actions": { "action": [{ "order": "0", "output-action": { "output-node-connector": "'''+outint+'''",
+                      "max-length": "60"}}]}}]}, "flow-name": "'''+flowname+'''", "installHw": "true", "strict": "false", "priority": "'''+flowpriority+'''",
+                      "idle-timeout": "0", "hard-timeout": "0", "table_id": "0"}]}'
+                      'http://localhost:8181/restconf/config/opendaylight-inventory:nodes/node/'''+dev+'''/flow-node-inventory:table/0/flow/'''+flowid+"'"'')
+
+            addmore = 'Add another flow?'
+            response = raw_input(addmore)
+            if (response == 'y' or 'Y'):
+                request = 'a'
+            elif (response == 'n' or 'N'):
+                true = 0
+                break
+
+        elif (request == 'm' or 'M'):
+            if (count == 0):
+                flowid = raw_input('Flow ID: ')
+                outint = raw_input('Egress interface: ')
+            elif (count == 1):
+                dev = raw_input('Enter the device you want to modify (openflow:x): ')
+                flowid = raw_input('Flow ID: ')
+                outint = raw_input('Egress interface: ')
+                break
+
+            os.system('''curl -u admin:admin -H 'Content-type: application/json' -X PUT -d '{ "instruction": [{ "order": "0", "apply-actions": { "action": [{ "order": "0",
+                      "output-action": { "output-node-connector": "'''+outint+'''", "max-length": "60"}}]}}]}'
+                      'http://localhost:8181/restconf/config/opendaylight-inventory:nodes/node/'''+dev+'''/flow-node-inventory:table/0/flow/'''+flowid+'''instructions/instruction/0''')
+            count += 1
+            modify = raw_input('Modify another flow?')
+            if (modify == 'y' or 'Y'):
+                request = 'm'
+            elif (modify == 'n' or 'N'):
+                true = 0
+                break
 createflows()
