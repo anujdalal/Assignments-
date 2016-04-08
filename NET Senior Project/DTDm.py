30174b915aa71e237b4fbe25d8ec7766b3c3feb3
Anuj Dalal <anujd744@gmail.com>
2016-04-07 20:15:05 -0400

    Final code (no comments)

--- NET Senior Project/DTDm.py
+++ NET Senior Project/DTDm.py
@@ -4,21 +4,21 @@ import time
 import urllib2
 
 def getbytesP(device, interface):
-
     auth_handler = urllib2.HTTPBasicAuthHandler()
     auth_handler.add_password(realm='application',
-                              uri='http://134.117.92.76:8181/restconf/operational/opendaylight-inventory:nodes/node/openflow:'+device+'/node-connector/openflow:'+device+':'+interface+'/opendaylight-port-statistics:flow-capable-node-connector-statistics/bytes',
+                              uri='http://localhost:8181/restconf/operational/opendaylight-inventory:nodes/node/openflow:'+device+'/node-connector/openflow:'+device+':'+interface+'/opendaylight-port-statistics:flow-capable-node-connector-statistics/bytes',
                               user='admin',
                               passwd='admin')
     opener = urllib2.build_opener(auth_handler)
     urllib2.install_opener(opener)
-    host = 'http://134.117.92.76:8181/restconf/operational/opendaylight-inventory:nodes/node/openflow:'+device+'/node-connector/openflow:'+device+':'+interface+'/opendaylight-port-statistics:flow-capable-node-connector-statistics/bytes'
+    host = 'http://localhost:8181/restconf/operational/opendaylight-inventory:nodes/node/openflow:'+device+'/node-connector/openflow:'+device+':'+interface+'/opendaylight-port-statistics:flow-capable-node-connector-statistics/bytes'
     req = urllib2.Request(host)
     nodeInfo = urllib2.urlopen(req)
     data = nodeInfo.read()
     sent = (re.findall('(?<="transmitted":)\w+', data))
     sent = map(int, sent)
     return sent
+
 
 def topo():
     dev = ['1', '3']
@@ -36,18 +36,18 @@ def topo():
             for index in range(len(int[1])):
                 interface = int[1][index]
                 data.extend(getbytesP(device, interface))
+    return data
+
 
 def DTD(outint,dev,flow1,flow2):
-
     flowid = [flow1,flow2]
     for index in range(len(flowid)):
         os.system('''curl -u admin:admin -H 'Content-type: application/json' -X PUT -d '{ "instruction": [{ "order": "0", "apply-actions": { "action": [{ "order": "0",
-                      "output-action": { "output-node-connector": "'''+outint+'''", "max-length": "60"}}]}}]}' 'http://134.117.92.76:8181/restconf/config/opendaylight-inventory:nodes/node/openflow:'''+dev+'''/flow-node-inventory:table/0/flow/'''+flowid[index]+'''/instructions/instruction/0' ''')
-
+                      "output-action": { "output-node-connector": "'''+outint+'''", "max-length": "60"}}]}}]}' 'http://localhost:8181/restconf/config/opendaylight-inventory:nodes/node/openflow:'''+dev+'''/flow-node-inventory:table/0/flow/'''+flowid[index]+'''/instructions/instruction/0' ''')
 
 
 def main():
-    print "Starting DTD application for mininet topology...\n"
+    print "Starting DTD application for physical topology...\n"
     data = topo()
     print "Monitoring  openflow:1:2 and openflow:3:1"
     state = [0,0]
@@ -57,13 +57,13 @@ def main():
                 outint = '2'
                 altint = '1'
                 flow1 = '5'
-                flow2 = '7'
+                flow2 = '6'
                 dev = '1'
             elif index is 1:
                 outint = '1'
                 altint = '2'
                 flow1 = '1'
-                flow2 = '3'
+                flow2 = '2'
                 dev = '3'
             sentBytes = data[index]
             while True:
@@ -71,23 +71,27 @@ def main():
                 data = topo()
                 sentBytes = data[index]
                 if sentBytes != oldSentBytes:
-                    byteRate = float(sentBytes - oldSentBytes)
+                    byteRate = round(float(sentBytes - oldSentBytes)/5,2)
                     print byteRate
                     util = round((byteRate/125000000)*100, 2)
                     print util, dev
-                    if util > 85 and state[index] is 0:
+                    if util > 90 and state[index] is 0:
                         DTD(altint,dev,flow1,flow2)
                         state[index] = 1
-                        print "Upper threshold reached on openflow:"+dev+":"+outint+", Utilization = "+str(util)+"%. All TCP traffic diverted to "+altint+"."
+                        print "Upper threshold reached on openflow:"+dev+":"+outint+", Utilization = "+str(util)+"%. All H1 traffic diverted to "+altint+"."
                     elif util < 50 and state[index] is 1:
                         DTD(outint,dev,flow1,flow2)
                         state[index] = 0
-                        print "Lower threshold reached on openflow:"+dev+":"+outint+", Utilization = "+str(util)+"%. All TCP traffic diverted back to "+outint+"."
-                else:
-                    True
+                        print "Lower threshold reached on openflow:"+dev+":"+outint+", Utilization = "+str(util)+"%. All H1 traffic diverted back to "+outint+"."
+		else:
+		    print "No change "+ dev
                 time.sleep(1)
                 break
 
 
 if __name__ == "__main__":
     main()
+
+
+
+
--- NET Senior Project/DTDp.py
+++ NET Senior Project/DTDp.py
@@ -6,12 +6,12 @@ import urllib2
 def getbytesP(device, interface):
     auth_handler = urllib2.HTTPBasicAuthHandler()
     auth_handler.add_password(realm='application',
-                              uri='http://134.117.92.76:8181/restconf/operational/opendaylight-inventory:nodes/node/openflow:'+device+'/node-connector/openflow:'+device+':'+interface+'/opendaylight-port-statistics:flow-capable-node-connector-statistics/bytes',
+                              uri='http://localhost:8181/restconf/operational/opendaylight-inventory:nodes/node/openflow:'+device+'/node-connector/openflow:'+device+':'+interface+'/opendaylight-port-statistics:flow-capable-node-connector-statistics/bytes',
                               user='admin',
                               passwd='admin')
     opener = urllib2.build_opener(auth_handler)
     urllib2.install_opener(opener)
-    host = 'http://134.117.92.76:8181/restconf/operational/opendaylight-inventory:nodes/node/openflow:'+device+'/node-connector/openflow:'+device+':'+interface+'/opendaylight-port-statistics:flow-capable-node-connector-statistics/bytes'
+    host = 'http://localhost:8181/restconf/operational/opendaylight-inventory:nodes/node/openflow:'+device+'/node-connector/openflow:'+device+':'+interface+'/opendaylight-port-statistics:flow-capable-node-connector-statistics/bytes'
     req = urllib2.Request(host)
     nodeInfo = urllib2.urlopen(req)
     data = nodeInfo.read()
@@ -36,18 +36,20 @@ def topo():
             for index in range(len(int[1])):
                 interface = int[1][index]
                 data.extend(getbytesP(device, interface))
+    return data
+
 
 def DTD(outint,dev,flow1,flow2):
     flowid = [flow1,flow2]
     for index in range(len(flowid)):
         os.system('''curl -u admin:admin -H 'Content-type: application/json' -X PUT -d '{ "instruction": [{ "order": "0", "apply-actions": { "action": [{ "order": "0",
-                      "output-action": { "output-node-connector": "'''+outint+'''", "max-length": "60"}}]}}]}' 'http://134.117.92.76:8181/restconf/config/opendaylight-inventory:nodes/node/openflow:'''+dev+'''/flow-node-inventory:table/0/flow/'''+flowid[index]+'''/instructions/instruction/0' ''')
+                      "output-action": { "output-node-connector": "'''+outint+'''", "max-length": "60"}}]}}]}' 'http://localhost:8181/restconf/config/opendaylight-inventory:nodes/node/openflow:'''+dev+'''/flow-node-inventory:table/0/flow/'''+flowid[index]+'''/instructions/instruction/0' ''')
 
 
 def main():
     print "Starting DTD application for physical topology...\n"
     data = topo()
-    print "Monitoring  openflow:11:5100 and openflow:3:5100"
+    print "Monitoring  openflow:11:5100 and openflow:13:5100"
     state = [0,0]
     while True:
         for index in range(len(data)):
@@ -55,13 +57,13 @@ def main():
                 outint = '5100'
                 altint = '5103'
                 flow1 = '9'
-                flow2 = '11'
+                flow2 = '10'
                 dev = '11'
             elif index is 1:
                 outint = '5100'
                 altint = '5099'
                 flow1 = '5'
-                flow2 = '7'
+                flow2 = '6'
                 dev = '13'
             sentBytes = data[index]
             while True:
@@ -69,11 +71,11 @@ def main():
                 data = topo()
                 sentBytes = data[index]
                 if sentBytes != oldSentBytes:
-                    byteRate = float(sentBytes - oldSentBytes)
+                    byteRate = float(sentBytes - oldSentBytes)/5
                     print byteRate
                     util = round((byteRate/125000000)*100, 2)
                     print util, dev
-                    if util > 85 and state[index] is 0:
+                    if util > 90 and state[index] is 0:
                         DTD(altint,dev,flow1,flow2)
                         state[index] = 1
                         print "Upper threshold reached on openflow:"+dev+":"+outint+", Utilization = "+str(util)+"%. All TCP traffic diverted to "+altint+"."
@@ -81,8 +83,6 @@ def main():
                         DTD(outint,dev,flow1,flow2)
                         state[index] = 0
                         print "Lower threshold reached on openflow:"+dev+":"+outint+", Utilization = "+str(util)+"%. All TCP traffic diverted back to "+outint+"."
-                else:
-                    True
                 time.sleep(1)
                 break
 
