2038df13eb63aaa81ef444f9babe5d09086013a1
anujdalal <anujd744@gmail.com>
2016-04-24 18:52:08 -0400

    Finalized and commented

--- NET Senior Project/DTDm.py
+++ NET Senior Project/DTDm.py
@@ -1,17 +1,32 @@
+#Author: Anuj Dalal
+#Team: Austin Toll, Ahmed Alabdulrahman, David Ballantyne
+#Written for the NET 4901 Senior Project – Dynamic Traffic Diversion in SDN
+#April 24th, 2016
+#Supervisor: Prof. Marc St-Hilaire – Carleton University
+
 import re
 import os
 import time
 import urllib2
 
-def getbytesP(device, interface):
+'''
+This function accesses the RESTful northboun interface of the controller, retrieves, and parses the interface data
+for the number of transmitted bytes
+'''
+def getbytes(device, interface):
+    #basic HTTP authentication
     auth_handler = urllib2.HTTPBasicAuthHandler()
     auth_handler.add_password(realm='application',
-                              uri='http://localhost:8181/restconf/operational/opendaylight-inventory:nodes/node/openflow:'+device+'/node-connector/openflow:'+device+':'+interface+'/opendaylight-port-statistics:flow-capable-node-connector-statistics/bytes',
+                              uri='http://localhost:8181/restconf/operational/opendaylight-inventory:nodes/node/'
+                                  'openflow:' + device + '/node-connector/openflow:' + device + ':' + interface +
+                                  '/opendaylight-port-statistics:flow-capable-node-connector-statistics/bytes',
                               user='admin',
                               passwd='admin')
     opener = urllib2.build_opener(auth_handler)
     urllib2.install_opener(opener)
-    host = 'http://localhost:8181/restconf/operational/opendaylight-inventory:nodes/node/openflow:'+device+'/node-connector/openflow:'+device+':'+interface+'/opendaylight-port-statistics:flow-capable-node-connector-statistics/bytes'
+    host = 'http://localhost:8181/restconf/operational/opendaylight-inventory:nodes/node/openflow:' + device + \
+           '/node-connector/openflow:' + device + ':' + interface + \
+           '/opendaylight-port-statistics:flow-capable-node-connector-statistics/bytes'
     req = urllib2.Request(host)
     nodeInfo = urllib2.urlopen(req)
     data = nodeInfo.read()
@@ -19,10 +34,12 @@ def getbytesP(device, interface):
     sent = map(int, sent)
     return sent
 
-
+'''
+For each device and interface in the relevant topology, get the interface statistics, and store them in an array
+'''
 def topo():
     dev = ['1', '3']
-    int = [['2'],['1']]
+    int = [['2'], ['1']]
     data = []
 
     for index in range(len(dev)):
@@ -30,27 +47,35 @@ def topo():
             device = dev[index]
             for index in range(len(int[0])):
                 interface = int[0][index]
-                data.extend(getbytesP(device, interface))
+                data.extend(getbytes(device, interface))
         elif index is 1:
             device = dev[index]
             for index in range(len(int[1])):
                 interface = int[1][index]
-                data.extend(getbytesP(device, interface))
+                data.extend(getbytes(device, interface))
     return data
 
-
-def DTD(outint,dev,flow1,flow2):
-    flowid = [flow1,flow2]
+'''
+Diverts appropriate flows to alternate interface using REST API and cURL
+'''
+def DTD(outint, dev, flow1, flow2):
+    flowid = [flow1, flow2]
     for index in range(len(flowid)):
-        os.system('''curl -u admin:admin -H 'Content-type: application/json' -X PUT -d '{ "instruction": [{ "order": "0", "apply-actions": { "action": [{ "order": "0",
-                      "output-action": { "output-node-connector": "'''+outint+'''", "max-length": "60"}}]}}]}' 'http://localhost:8181/restconf/config/opendaylight-inventory:nodes/node/openflow:'''+dev+'''/flow-node-inventory:table/0/flow/'''+flowid[index]+'''/instructions/instruction/0' ''')
+        os.system('''curl -u admin:admin -H 'Content-type: application/json' -X PUT -d '{ "instruction":
+        [{ "order": "0", "apply-actions": { "action": [{ "order": "0",
+        "output-action": { "output-node-connector": "''' + outint + '''", "max-length": "60"}}]}}]}'
+        'http://localhost:8181/restconf/config/opendaylight-inventory:nodes/node/openflow:''' + dev +
+        '''/flow-node-inventory:table/0/flow/''' +
+        flowid[index] + '''/instructions/instruction/0' ''')
+
+    print  "!!!Traffic for flows " + flow1 + " and flow " + flow2 + " diverted to alternate link!!!"
 
 
 def main():
     print "Starting DTD application for physical topology...\n"
-    data = topo()
+    data = topo() #get initial topoology data. 2 position is this array: 0 = S1, 1 = S3
     print "Monitoring  openflow:1:2 and openflow:3:1"
-    state = [0,0]
+    state = [0, 0] #state for both devices is "no flows have been diverted"
     while True:
         for index in range(len(data)):
             if index is 0:
@@ -59,39 +84,38 @@ def main():
                 flow1 = '5'
                 flow2 = '6'
                 dev = '1'
+                devname = 'Switch 1'
             elif index is 1:
                 outint = '1'
                 altint = '2'
                 flow1 = '1'
                 flow2 = '2'
                 dev = '3'
+                devname = 'Switch 3'
             sentBytes = data[index]
             while True:
                 oldSentBytes = sentBytes
                 data = topo()
                 sentBytes = data[index]
                 if sentBytes != oldSentBytes:
-                    byteRate = round(float(sentBytes - oldSentBytes)/5,2)
+                     '''Cisco switches take ~ 5 seconds to locally update byte values. Therefore number of bytes between
+                       intervals / 5 = byterate in Bps'''
+                    byteRate = float(sentBytes - oldSentBytes)
                     print byteRate
-                    util = round((byteRate/125000000)*100, 2)
+                    util = round((byteRate / 125000000) * 100, 2) #125000000 = 1 Gbit
                     print util, dev
                     if util > 90 and state[index] is 0:
-                        DTD(altint,dev,flow1,flow2)
+                        print "!!!Upper threshold reached on " + devname + ": " + outint + ", Utilization = " + \
+                              str(util) + "%!!!\n"
+                        DTD(altint, dev, flow1, flow2)
                         state[index] = 1
-                        print "Upper threshold reached on openflow:"+dev+":"+outint+", Utilization = "+str(util)+"%. All H1 traffic diverted to "+altint+"."
+
                     elif util < 50 and state[index] is 1:
-                        DTD(outint,dev,flow1,flow2)
+                        print "!!!Lower threshold reached on " + devname + ": " + outint + ", Utilization = " + \
+                              str(util) + "%!!!\n"
+                        DTD(outint, dev, flow1, flow2)
                         state[index] = 0
-                        print "Lower threshold reached on openflow:"+dev+":"+outint+", Utilization = "+str(util)+"%. All H1 traffic diverted back to "+outint+"."
-		else:
-		    print "No change "+ dev
                 time.sleep(1)
                 break
-
-
 if __name__ == "__main__":
     main()
-
-
-
-
--- NET Senior Project/DTDp.py
+++ NET Senior Project/DTDp.py
@@ -1,18 +1,34 @@
+# Author: Anuj Dalal
+# Team: Austin Toll, Ahmed Alabdulrahman, David Ballantyne
+# Written for the NET 4901 Senior Project – Dynamic Traffic Diversion in SDN
+# April 24th, 2016
+# Supervisor: Prof. Marc St-Hilaire – Carleton University
 
 import re
 import os
 import time
 import urllib2
 
-def getbytesP(device, interface):
+'''
+This function accesses the RESTful northboun interface of the controller, retrieves, and parses the interface data
+for the number of transmitted bytes
+'''
+
+
+def getbytes(device, interface):
+    # basic HTTP authentication
     auth_handler = urllib2.HTTPBasicAuthHandler()
     auth_handler.add_password(realm='application',
-                              uri='http://localhost:8181/restconf/operational/opendaylight-inventory:nodes/node/openflow:'+device+'/node-connector/openflow:'+device+':'+interface+'/opendaylight-port-statistics:flow-capable-node-connector-statistics/bytes',
+                              uri='http://localhost:8181/restconf/operational/opendaylight-inventory:nodes/node/'
+                                  'openflow:' + device + '/node-connector/openflow:' + device + ':' + interface +
+                                  '/opendaylight-port-statistics:flow-capable-node-connector-statistics/bytes',
                               user='admin',
                               passwd='admin')
     opener = urllib2.build_opener(auth_handler)
     urllib2.install_opener(opener)
-    host = 'http://localhost:8181/restconf/operational/opendaylight-inventory:nodes/node/openflow:'+device+'/node-connector/openflow:'+device+':'+interface+'/opendaylight-port-statistics:flow-capable-node-connector-statistics/bytes'
+    host = 'http://localhost:8181/restconf/operational/opendaylight-inventory:nodes/node/openflow:' + device + \
+           '/node-connector/openflow:' + device + ':' + interface + \
+           '/opendaylight-port-statistics:flow-capable-node-connector-statistics/bytes'
     req = urllib2.Request(host)
     nodeInfo = urllib2.urlopen(req)
     data = nodeInfo.read()
@@ -21,9 +37,14 @@ def getbytesP(device, interface):
     return sent
 
 
+'''
+For each device and interface in the relevant topology, get the interface statistics, and store them in an array
+'''
+
+
 def topo():
     dev = ['11', '13']
-    int = [['5100'],['5100']]
+    int = [['5100'], ['5100']]
     data = []
 
     for index in range(len(dev)):
@@ -31,67 +52,79 @@ def topo():
             device = dev[index]
             for index in range(len(int[0])):
                 interface = int[0][index]
-                data.extend(getbytesP(device, interface))
+                data.extend(getbytes(device, interface))
         elif index is 1:
             device = dev[index]
             for index in range(len(int[1])):
                 interface = int[1][index]
-                data.extend(getbytesP(device, interface))
+                data.extend(getbytes(device, interface))
     return data
 
 
-def DTD(outint,dev,flow1,flow2):
-    flowid = [flow1,flow2]
+'''
+Diverts appropriate flows to alternate interface using REST API and cURL
+'''
+
+
+def DTD(outint, dev, flow1, flow2):
+    flowid = [flow1, flow2]
     for index in range(len(flowid)):
-        os.system('''curl -u admin:admin -H 'Content-type: application/json' -X PUT -d '{ "instruction": [{ "order": "0", "apply-actions": { "action": [{ "order": "0",
-                      "output-action": { "output-node-connector": "'''+outint+'''", "max-length": "60"}}]}}]}' 'http://localhost:8181/restconf/config/opendaylight-inventory:nodes/node/openflow:'''+dev+'''/flow-node-inventory:table/0/flow/'''+flowid[index]+'''/instructions/instruction/0' ''')
-    print  "!!!Traffic for flows "+flow1+" and flow "+flow2+" diverted to alternate link!!!"
+        os.system('''curl -u admin:admin -H 'Content-type: application/json' -X PUT -d '{ "instruction":
+        [{ "order": "0", "apply-actions": { "action": [{ "order": "0",
+        "output-action": { "output-node-connector": "''' + outint + '''", "max-length": "60"}}]}}]}'
+        'http://localhost:8181/restconf/config/opendaylight-inventory:nodes/node/openflow:''' + dev +
+                  '''/flow-node-inventory:table/0/flow/''' +
+                  flowid[index] + '''/instructions/instruction/0' ''')
+
+    print  "!!!Traffic for flows " + flow1 + " and flow " + flow2 + " diverted to alternate link!!!"
+
 
 def main():
     print "Starting DTD application for physical topology...\n"
-    data = topo()
+    data = topo()  # get initial topoology data. 2 position is this array: 0 = S1, 1 = S3
     print "Monitoring Switch 1: Gi1/0/23 and Switch 3: Gi1/0/24\n"
-    state = [0,0]
+    state = [0, 0]  # state for both devices is "no flows have been diverted"
     while True:
         for index in range(len(data)):
             if index is 0:
                 outint = '5100'
-		intname = 'Gi1/0/23'
+                intname = 'Gi1/0/23'
                 altint = '5103'
-		altintname= 'Gi1/0/24'
+                altintname = 'Gi1/0/24'
                 flow1 = '9'
                 flow2 = '10'
                 dev = '11'
-		devname = 'Switch 1'
+                devname = 'Switch 1'
             elif index is 1:
                 outint = '5100'
-		intname = 'Gi1/0/24'
+                intname = 'Gi1/0/24'
                 altint = '5099'
-		altintname = 'Gi1/0/23'
+                altintname = 'Gi1/0/23'
                 flow1 = '5'
                 flow2 = '6'
                 dev = '13'
-		devname = 'Switch 3'
+                devname = 'Switch 3'
             sentBytes = data[index]
             while True:
                 oldSentBytes = sentBytes
                 data = topo()
                 sentBytes = data[index]
                 if sentBytes != oldSentBytes:
-                    byteRate = float(sentBytes - oldSentBytes)/5
-                    #print byteRate
-                    util = round((byteRate/125000000)*100, 2)
-		    print "Utilizaition = "+str(util)+"% on "+devname+": "+intname+"\n"
+                    '''Cisco switches take ~ 5 seconds to locally update byte values. Therefore number of bytes between
+                       intervals / 5 = byterate in Bps'''
+                    byteRate = float(sentBytes - oldSentBytes) / 5
+                    util = round((byteRate / 125000000) * 100, 2)  # 125000000 = 1 Gbit
+                    print "Utilizaition = " + str(util) + "% on " + devname + ": " + intname + "\n"
                     if util > 90 and state[index] is 0:
-			print "!!!Upper threshold reached on "+devname+": "+intname+", Utilization = "+str(util)+"%!!!\n"
-                        DTD(altint,dev,flow1,flow2)
+                        print "!!!Upper threshold reached on " + devname + ": " + intname + ", Utilization = " + str(
+                            util) + "%!!!\n"
+                        DTD(altint, dev, flow1, flow2)
                         state[index] = 1
                     elif util < 50 and state[index] is 1:
-			print "!!!Lower threshold reached on "+devname+": "+intname+", Utilization = "+str(util)+"%!!!\n"
-                        DTD(outint,dev,flow1,flow2)
+                        print "!!!Lower threshold reached on " + devname + ": " + intname + ", Utilization = " + str(
+                            util) + "%!!!\n"
+                        DTD(outint, dev, flow1, flow2)
                         state[index] = 0
-		#else:
-		#    print "No change on "+devname
                 time.sleep(0.5)
                 break
 
